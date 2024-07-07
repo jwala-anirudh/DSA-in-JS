67788f63a37130a6960c5efffbc3a98c43af4499
Anirudh Jwala <anirudhsai2@gmail.com>
2024-07-07 22:35:11 +0530

    doubly linked list

---
+++ 04doublylinkedlist/02insert.js
@@ -0,0 +1,40 @@
+class Node {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+
+DoublyLinkedList.prototype.insertAtBeginning = function (data) {
+  const newNode = new Node(data, this.head, null);
+
+  if (this.head !== null) {
+    this.head.prev = newNode;
+  }
+
+  this.head = newNode;
+  if (this.tail === null) {
+    this.tail = newNode;
+  }
+};
+
+DoublyLinkedList.prototype.insertAtEnd = function (data) {
+  const newNode = new Node(data, null, this.tail);
+
+  if (this.tail !== null) {
+    this.tail.next = newNode;
+  }
+
+  this.tail = newNode;
+  if (this.head === null) {
+    this.head = newNode;
+  }
+};
