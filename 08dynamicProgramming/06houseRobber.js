040ea5d487b7d502482c0c104dd9f7e139d15bc7
Anirudh Jwala <anirudhsai2@gmail.com>
2024-08-30 22:17:27 +0530

    implement houseRobber function using dynamic programming

---
+++ 08dynamicProgramming/06houseRobber.js
@@ -0,0 +1,17 @@
+function houseRobber(nums, i = 0, memo = {}) {
+  if (i in memo) {
+    return memo[i];
+  }
+
+  // reached the end
+  if (i >= nums.length) {
+    return 0;
+  }
+
+  const skip = houseRobber(nums, i + 1, memo);
+  const take = nums[i] + houseRobber(nums, i + 2, memo);
+
+  memo[i] = Math.max(skip, take);
+
+  return memo[i];
+}
