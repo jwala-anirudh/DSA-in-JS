48a15d961ca8479fef4f38a1b56c96faa12114c0
Anirudh Jwala <anirudhsai2@gmail.com>
2024-09-03 01:22:14 +0530

    implementation of kidsWithCandies

---
+++ 09leetcode/03greatestCandies.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ *
+ * There are n kids with candies. You are given an integer array candies,
+ * where each candies[i] represents the number of candies the ith kid has,
+ * and an integer extraCandies, denoting the number of extra candies
+ * that you have.
+ *
+ * Return a boolean array result of length n, where result[i] is true
+ * if, after giving the ith kid all the extraCandies, they will have
+ * the greatest number of candies among all the kids, or false otherwise.
+ *
+ * Note that multiple kids can have the greatest number of candies.
+ */
+function kidsWithCandies(candies, extraCandies) {
+  const maxNoOfCandies = Math.max(...candies);
+
+  return candies.map((candy) => candy + extraCandies >= maxNoOfCandies);
+}
