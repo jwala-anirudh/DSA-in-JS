4f540685cd4935be87bf9a7b02d260e9cd5be7b1
Anirudh Jwala <anirudhsai2@gmail.com>
2024-09-07 16:28:53 +0530

    Implement string compression algorithm

---
+++ 09leetcode/09stringCompress.js
@@ -0,0 +1,47 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ *
+ * Given an array of characters chars, compress it using the following algorithm:
+ *
+ * Begin with an empty string s. For each group of consecutive repeating characters in chars:
+ *
+ * - If the group's length is 1, append the character to s.
+ * - Otherwise, append the character followed by the group's length.
+ *
+ * The compressed string s should not be returned separately, but instead,
+ * be stored in the input character array chars. Note that group lengths that
+ * are 10 or longer will be split into multiple characters in chars.
+ *
+ * After you are done modifying the input array, return the new length of the array.
+ *
+ * You must write an algorithm that uses only constant extra space.
+ */
+function compress(chars) {
+  let index = 0;
+  let i = 0;
+
+  while (i < chars.length) {
+    let j = i;
+
+    while (j < chars.length && chars[j] === chars[i]) {
+      j++;
+    }
+
+    let count = j - i;
+
+    chars[index] = chars[i];
+    index += 1;
+
+    if (count > 1) {
+      for (const c of count.toString()) {
+        chars[index] = c;
+        index += 1;
+      }
+    }
+
+    i = j;
+  }
+
+  return index;
+}
