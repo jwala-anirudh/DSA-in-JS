0f7b5d9815713c19a53260a392c2914092c414e4
Anirudh Jwala <anirudhsai2@gmail.com>
2024-09-18 01:27:00 +0530

    Add findMaxAverage function to calculate the maximum average value of a contiguous subarray of length k in an integer array

---
+++ 09leetcode/03-sliding-window/01maxAvgSubarray.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ *
+ * You are given an integer array nums consisting of
+ * n elements, and an integer k.
+ *
+ * Find a contiguous subarray whose length is equal to k
+ * that has the maximum average value and return this value.
+ *
+ * Any answer with a calculation error less than 10^-5 will be accepted.
+ */
+function findMaxAverage(nums, k) {
+  let sum = 0;
+  // calculate initial sum of k-elements
+  for (let i = 0; i < k; i++) {
+    sum += nums[i];
+  }
+
+  let maxSum = sum;
+  // slide the window to check other maximums if present
+  for (let i = k; i < nums.length; i++) {
+    sum = sum - nums[i - k] + nums[i];
+    maxSum = Math.max(sum, maxSum);
+  }
+
+  return maxSum / k;
+}
